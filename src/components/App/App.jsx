2f15cba253af9c1e377ebeffcf254b99b6922ddd
DmitryMuzalev <56193550+DmitryMuzalev@users.noreply.github.com>
2023-06-30 15:41:49 +0300

    Implementation of header and switching between theme modes

---
+++ src/components/App/App.jsx
@@ -0,0 +1,14 @@
+import { Header } from '../Heder/Heder';
+
+function App() {
+  return (
+    <>
+      <div className={'curtain'}></div>
+      <div className="container">
+        <Header />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Heder/Heder.jsx
@@ -0,0 +1,25 @@
+import classes from './Header.module.scss';
+import { useEffect, useState } from 'react';
+import { BsSunFill, BsMoonFill } from 'react-icons/bs';
+
+function Header() {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme);
+  }, [theme]);
+
+  const onToggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>TODO</h1>
+      <div onClick={onToggleTheme} className={classes.switcher}>
+        {theme === 'dark' ? <BsSunFill /> : <BsMoonFill />}
+      </div>
+    </header>
+  );
+}
+export { Header };
